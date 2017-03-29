d8a1db36e17450715a6d2c7b56a621836c471ab4
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-03-29 12:09:01 +0100

    Build to deploy

---
+++ src/components/NewHaiku.vue
@@ -0,0 +1,122 @@
+<template lang="html">
+  <div class="newHaiku">
+    <form class="newHaikuForm">
+      <div>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" v-model="title">
+      </div>
+      <div>
+        <label for="image-path">Image path</label>
+        <input type="text" name="image-path" id="image-path" v-model="imagePath" v-on:change="setImgUrl">
+      </div>
+      <div class='thumb'>
+        <img :src="imageUrl" v-bind:alt="title">
+      </div>
+      <div>
+        <label for="haiku">Haiku/verse</label>
+        <textarea name="haiku" rows="4" id="haiku" cols="80" v-model="haiku"></textarea>
+      </div>
+      <div>
+        <label for="comment">Comment</label>
+        <textarea name="comment" rows="4" id="comment" cols="80" v-model="comment"></textarea>
+      </div>
+      <div>
+        <label for='isPriority'>Prioritized</label>
+        <input type="checkbox" id='isPriority' v-model='isPriority'>
+      </div>
+      <div>
+        <label for='display'>Display</label>
+        <input type="checkbox" class='display' v-model='display'>
+      </div>
+      <div>
+        <button type="button" name="button" id="save" @click="save">Save</button>
+      </div>
+
+    </form>
+  </div>
+</template>
+
+<script>
+var axios = require('axios');
+if (process.env.NODE_ENV === 'development') {
+  let root = 'http://localhost:8081/api/'
+}
+
+export default {
+  data() {
+    return {
+      title: '',
+      imagePath: '',
+      imageUrl: '',
+      haiku: '',
+      comment: '',
+      isPriority: false,
+      display: true,
+      created: new Date()
+    }
+  },
+  mounted() {
+    if (this.hasToken() === false) {
+      this.$router.push('/auth')
+    }
+  },
+  methods: {
+    hasToken() {
+      let token = localStorage.getItem('haikuToken');
+
+      if (token) {
+        console.log('Has Token');
+        return true;
+      } else {
+        console.log('No token');
+        return false;
+      }
+    },
+    setImgUrl() {
+      alert('setImgUrl')
+      let root = 'http://res.cloudinary.com/ginkgo/image/upload/w_180/'
+      this.imageUrl = root + this.imagePath
+      alert(this.imageUrl)
+    },
+    save() {
+      let token = localStorage.getItem('haikuToken');
+      axios.defaults.headers.common ['x-auth']  = token
+      axios.post(root + 'haiku', {
+          title: this.title,
+          imagePath: this.imagePath,
+          haiku: this.haiku,
+          comment: this.comment,
+          isPriority: this.isPriority,
+          display: this.display,
+          created: this.created
+        })
+        .then((res)=>{
+          console.log(res);
+          this.title = '';
+          this.imagePath = '';
+          this.haiku = '';
+          this.comment = '';
+          this.isPriority = false;
+          this.display = true;
+          this.imageUrl = '';
+        })
+        .catch((err)=>{
+          console.log(err);
+        });
+      }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+.newHaikuForm {
+  position: absolute;
+  margin: auto;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 60%;
+  height: 60%
+}
+</style>
